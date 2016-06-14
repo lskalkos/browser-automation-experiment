18cd63692c5d4f24c3b9ee6325bad016116fac13
Lia Skalkos <hello@liaskalkos.net>
2016-06-14 07:26:10 -0400

    use capybara driver instead of selenium

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 gem 'byebug'
 gem 'browsermob-proxy'
 gem 'selenium-webdriver'
+gem 'capybara'
--- experiment.rb
+++ experiment.rb
@@ -1,8 +1,11 @@
 require 'selenium-webdriver'
 require 'byebug'
 require 'browsermob-proxy'
+require 'capybara'
+require 'capybara/dsl'
 
 class EdgeTest
+  include Capybara::DSL
   attr_accessor :url
 
   def initialize(url)
@@ -13,11 +16,13 @@ class EdgeTest
 
   def run
     server.start
+    setup_driver
     proxy.new_har("#{url}")
-    driver.get(url)
+    visit url
+    # driver.get(url)
     har.save_to("#{temp_file}")
     proxy.close
-    driver.quit
+    # driver.quit
     `har #{temp_file}`
   end
 
@@ -25,10 +30,17 @@ class EdgeTest
     @server ||= BrowserMob::Proxy::Server.new("./browsermob-proxy-2.1.1/bin/browsermob-proxy", log: true)
   end
 
-  def driver
+  def setup_driver
     selenium_proxy = Selenium::WebDriver::Proxy.new(:http => proxy.selenium_proxy.http)
     caps = Selenium::WebDriver::Remote::Capabilities.chrome(:proxy => selenium_proxy)
-    @driver ||= Selenium::WebDriver.for(:chrome, :desired_capabilities => caps)
+
+    Capybara.register_driver :chrome do |app|
+      Capybara::Selenium::Driver.new(app, :browser => :chrome, :desired_capabilities => caps)
+    end
+
+    Capybara.current_driver = :chrome
+
+    # @driver ||= Selenium::WebDriver.for(:chrome, :desired_capabilities => caps)
   end
 
 
